521d1d91b014847047d10e4f38f3c2c75fea577a
mahdibough6 <mahdibough6@gmail.com>
2022-11-28 20:13:14 +0100

    firstCommit

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
 <?php
+
+$server = "localhost";
+$user = "root";
+$pwd = "";
+$db = "";
+
+$con = mysqli_connect($server, $user, $pwd, $db);
+
+if(!$con){
+    die("Connectino Failed :" . mysqli_connect_error());
+}
+
--- signup.php
+++ signup.php
@@ -1,1 +1,12 @@
-<?php
+<section>
+    <h2>sign up</h2>
+    <form action="/includes/signup.inc.php" method="post">
+        <p>enter your username :</p>
+        <input type="text" name="username" placeholder="Ex: blabla123">
+        <p>enter your password :</p>
+        <input type="password" name="password" placeholder="********">
+        <button type="submit">sign up</button>
+
+    </form>
+    
+</section>
